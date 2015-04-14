e1b453ba7a09daabd3bbd135f908c9b95a0f3c9a
zhengsl <zhengsl@tongxinyiliao.com>
2015-04-14 17:46:33 +0800

    Fresco：1.xml设置图片圆角

--- app/src/main/java/com/zhengsonglan/frescodemo/xml/XmlActivity.java
+++ app/src/main/java/com/zhengsonglan/frescodemo/xml/XmlActivity.java
@@ -14,14 +14,13 @@ import com.zhengsonglan.frescodemo.R;
 
 public class XmlActivity extends ActionBarActivity implements View.OnClickListener {
     //initView
-    Button bt_xml,bt_xml_failure;
+    Button bt_xml, bt_xml_failure;
     SimpleDraweeView simpleDraweeView;
 
     //initData
     Intent intent = new Intent();
-    String uri="http://img4.imgtn.bdimg.com/it/u=3399249321,1109833355&fm=21&gp=0.jpg";
-    String uri_failure="http://img4.imgtn.bdimg.com/it/u=3399249321,1109833355&fm";
-
+    String uri = "http://img4.imgtn.bdimg.com/it/u=3399249321,1109833355&fm=21&gp=0.jpg";
+    String uri_failure = "http://img4.imgtn.bdimg.com/it/u=3399249321,1109833355&fm";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,20 +28,22 @@ public class XmlActivity extends ActionBarActivity implements View.OnClickListen
         initView();
         initEvent();
 
+
     }
 
 
     //初始化view
     private void initView() {
         bt_xml = (Button) findViewById(R.id.xml_bt_xml);
-        bt_xml_failure= (Button) findViewById(R.id.xml_bt_xml_failure);
-        simpleDraweeView= (SimpleDraweeView) findViewById(R.id.xml_iv_show);
+        bt_xml_failure = (Button) findViewById(R.id.xml_bt_xml_failure);
+        simpleDraweeView = (SimpleDraweeView) findViewById(R.id.xml_iv_show);
     }
 
     private void initEvent() {
         bt_xml.setOnClickListener(this);
         bt_xml_failure.setOnClickListener(this);
     }
+
     //点击事件
     @Override
     public void onClick(View v) {
