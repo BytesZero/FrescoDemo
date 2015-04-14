a000ff1ddf62ccf18674c76d6d8c1cfdc11f82e2
zhengsl <zhengsl@tongxinyiliao.com>
2015-04-14 15:00:23 +0800

    初始化Fresco

--- app/src/main/java/com/zhengsonglan/frescodemo/MainActivity.java
+++ app/src/main/java/com/zhengsonglan/frescodemo/MainActivity.java
@@ -3,12 +3,17 @@ package com.zhengsonglan.frescodemo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //≥ı ºªØfresco
+        Fresco.initialize(MainActivity.this);
+
         setContentView(R.layout.activity_main);
     }
 
